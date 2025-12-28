cb54ddc90f157e0cdef61913a569458eac47f8f6
misaelavelino568@gmail.com <misaelavelino568@gmail.com>
2025-12-28 22:00:23 +0100

    change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("loginForm").onsubmit = function(e) {
     e.preventDefault(); // impede o envio automático
-
+  
     // COMO TODOS OS CAMPOS TÊM "required", só chega aqui se tudo estiver preenchido
     window.location.href = "Pagina_inicial.html";
   };
