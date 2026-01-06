4c0fdfd7a2a415757fa913341f44680da01c56f5
Pedro Mawonso <misaelavelino568@gmail.com>
2026-01-06 16:57:40 +0100

    Feito alteracao e terminado a pagina inicial

---
+++ Animacao.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll('a.link')
+
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault(); // Essa parte evita o carregamento imediato
+        document.body.classList.add('opacity-0')
+        setTimeout(() => {
+            window.location.href = link.href
+        }, 3000)
+    })
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.remove('opacity-0')
+})
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("loginForm").onsubmit = function(e) {
     e.preventDefault(); // impede o envio automático
     // COMO TODOS OS CAMPOS TÊM "required", só chega aqui se tudo estiver preenchido
-    window.location.href = "Pagina_inicial.html";
+    window.location.href = "Transicao.html";
   };
 
   // Funcionalidade de ver e ocultar senha
